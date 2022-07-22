37e369409e30742ef7ba13520c9576b532071e2f
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2022-07-22 17:18:07 +0900

    add edge support

--- apps/documentation/svelte.config.js
+++ apps/documentation/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,7 +12,9 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			edge: true,
+		}),
 		files: {
 			lib: 'src/lib'
 		},
