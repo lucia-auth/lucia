241b66475ec4050d5d127a3f24bac6263b31a5f1
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2022-09-27 23:58:27 +0900

    add base setup for new docs

---
+++ apps/documentation-beta/astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
+import svelte from "@astrojs/svelte";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), svelte()]
+});
