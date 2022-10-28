5196e54813fb9fc8fc779947336cc60db04e5677
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2022-10-28 22:07:56 +0900

    update example

--- examples/sveltekit/username-password-supabase/src/routes/+page.svelte
+++ examples/sveltekit/username-password-supabase/src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<h1>Lucia username+password demo</h1>
+<h1>Lucia username+password example</h1>
 <div>
     <a href="/login" class="button">Sign in</a>
     <a href="/signup" class="button">Create an account</a>
