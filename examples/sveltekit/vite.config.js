5da89532c262c1bbabad7a2b842f8db9e2e2fdc2
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2022-11-15 17:07:30 +0900

    fix sveltekit example vite config

--- examples/sveltekit/vite.config.js
+++ examples/sveltekit/vite.config.js
@@ -2,7 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	ssr: {
+		noExternal: ['@lucia-auth/sveltekit']
+	}
 };
 
 export default config;
