d392dbbda4863e59ffc167030c4586d2cc889a0e
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2022-11-22 22:24:56 -0500

    added state validation

--- examples/oauth/src/pages/api/github.ts
+++ examples/oauth/src/pages/api/github.ts
@@ -3,17 +3,23 @@ import { auth, githubAuth } from "../../lib/lucia";
 import type { APIRoute } from "astro";
 
 export const get: APIRoute = async ({ url, request, cookies }) => {
-	const { existingUser, createUser, providerUser } = await githubAuth.validateCallback(
-		url.searchParams.get("code") || ""
-	);
+	const code = url.searchParams.get("code");
+	const state = url.searchParams.get("state");
+	if (state !== cookies.get("githubState").value || !code) {
+		return new Response("Invalid state or code", {
+			status: 400
+		});
+	}
+
+	const { existingUser, createUser, providerUser } = await githubAuth.validateCallback(code || "");
 	const user =
 		existingUser ||
 		(await createUser({
 			username: providerUser.login
 		}));
 	const session = await auth.createSession(user.userId);
-	const authRequest = new AuthRequest(auth, {request, cookies})
-	authRequest.setSession(session)
+	const authRequest = new AuthRequest(auth, { request, cookies });
+	authRequest.setSession(session);
 	return new Response(null, {
 		status: 302,
 		headers: {
--- examples/oauth/src/pages/api/logout.ts
+++ examples/oauth/src/pages/api/logout.ts
@@ -1,5 +1,5 @@
-import { handleSignOut } from "@lucia-auth/astro";
+import { handleLogoutRequests } from "@lucia-auth/astro";
 import { auth } from "../../lib/lucia";
 import type { APIRoute } from "astro";
 
-export const post: APIRoute = handleSignOut(auth);
+export const post: APIRoute = handleLogoutRequests(auth);
