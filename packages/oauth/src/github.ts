93d0d0c510ead25f378e4de353529eeb7ce2288c
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2022-11-25 09:30:57 -0500

    fixed state

--- packages/oauth/src/github.ts
+++ packages/oauth/src/github.ts
@@ -31,7 +31,7 @@ class Github<A extends Auth> implements OAuthProvider {
 			...(s && { state: s })
 		}).toString()}`;
 		if (state === null) return [url] as const as GetAuthorizationUrlReturnType<State>;
-		return [url, state] as const as GetAuthorizationUrlReturnType<State>;
+		return [url, s] as const as GetAuthorizationUrlReturnType<State>;
 	};
 
 	public validateCallback = async (code: string) => {
