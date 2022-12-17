dda18156ef0394c1050d784db8076ce89fb7aedc
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2022-12-17 17:02:41 +0900

    fix landing page breakpoint

--- documentation/src/components/icon-links/MenuButton.svelte
+++ documentation/src/components/icon-links/MenuButton.svelte
@@ -1,19 +1,15 @@
 <script lang="ts">
 	import { isMenuOpen } from "src/lib/stores";
-	export let pathname: string;
-
-	const hideOnLg = pathname === "/";
 </script>
 
 <button
 	on:click={() => {
 		isMenuOpen.toggle();
 	}}
-	class:lg:hidden={hideOnLg}
-	class:xl:hidden={!hideOnLg}
+	class="xl:hidden"
 >
 	<div class="h-8 w-8 dark:text-zinc-200 fill-current">
-		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" class="w-full h-full"
 			><path d="M6 36v-3h36v3Zm0-10.5v-3h36v3ZM6 15v-3h36v3Z" />
 		</svg>
 	</div>
