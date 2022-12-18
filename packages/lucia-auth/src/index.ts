27d2d1a28a38e75fc05882a5115faad181af31e6
Oscar Beaumont <oscar@otbeaumont.me>
2022-12-18 23:19:13 +0800

    export `Configurations` type in `src/index.ts`

--- packages/lucia-auth/src/index.ts
+++ packages/lucia-auth/src/index.ts
@@ -1,4 +1,4 @@
-export { lucia as default, SESSION_COOKIE_NAME } from "./auth/index.js";
+export { lucia as default, Configurations, SESSION_COOKIE_NAME } from "./auth/index.js";
 export { LuciaError } from "./auth/error.js";
 export { generateRandomString } from "./utils/crypto.js";
 export { serializeCookie } from "./utils/cookie.js";
