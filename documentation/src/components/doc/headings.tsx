e98fb6162cb0c77236acc825fb5933958fd240ec
pilcrowOnPaper <80624252+pilcrowOnPaper@users.noreply.github.com>
2023-02-11 20:38:14 +0900

    Improve accessibility (#345)

--- documentation/src/components/doc/headings.tsx
+++ documentation/src/components/doc/headings.tsx
@@ -7,7 +7,7 @@ export default (props: { headings: MarkdownHeading[] }) => {
 		<div class="hidden lg:block pt-36 lg:pt-20 fixed right-0 top-0 w-64 h-screen mr-4 sm:mr-8 lg:mr-12">
 			<div class="overflow-auto h-full relative overscroll-contain pl-4">
 				<div class="w-full sticky top-0 bg-white dark:bg-black-zinc">
-					<h3 class="font-medium">On this page</h3>
+					<h2 class="font-medium">On this page</h2>
 				</div>
 				<div class="pt-4 pb-8">
 					<ul class="list-none text-gray-500 dark:text-zinc-400">
