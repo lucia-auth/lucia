855f1d06dff4b9f2f6d8b9ff1e75f237af224164
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2023-02-13 18:29:46 +0900

    integrate siena

--- documentation/siena/README.md
+++
@@ -1,9 +0,0 @@
-# Siena
-
-This is a super simple script to make it possible to use relative images inside markdown files, and optimize those images on build.
-
-### Build
-
-```
-pnpm siena
-```
--- documentation/siena/index.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Root } from "hast";
-import type { VFile } from "vfile";
-type AstroVFile = Omit<VFile, "data"> & {
-    data: {
-        astro: Record<string, any>;
-    };
-};
-declare const _default: () => (root: Root, file: AstroVFile) => Promise<void>;
-export default _default;
