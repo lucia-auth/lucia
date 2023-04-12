b4cd43c92c4e64e35f68ec2eb733b67509cd8726
Stewartside <rhys.stewart1@gmail.com>
2023-04-13 00:06:01 +0100

    Fix documentation: Minor clean up on docs for incorrect names/urls (#496)

--- examples/sveltekit-email/src/lib/lucia.ts
+++ examples/sveltekit-email/src/lib/lucia.ts
@@ -1,13 +1,13 @@
 import lucia from 'lucia-auth';
 import prisma from '@lucia-auth/adapter-prisma';
-import { astro } from 'lucia-auth/middleware';
+import { sveltekit } from 'lucia-auth/middleware';
 import { idToken } from '@lucia-auth/tokens';
 import { prismaClient } from '$lib/db';
 
 export const auth = lucia({
 	env: import.meta.env.DEV ? 'DEV' : 'PROD',
 	adapter: prisma(prismaClient),
-	middleware: astro(),
+	middleware: sveltekit(),
 	transformDatabaseUser: (userData) => {
 		return {
 			userId: userData.id,
