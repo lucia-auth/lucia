0cfe3c02f485d9a03cfa07a708a328b8d559648c
gustavocadev <churaalaygustavo11@gmail.com>
2023-04-30 15:29:24 -0500

    chore: use validate instead of validateUser fn

--- examples/qwik/src/routes/signup/index.tsx
+++ examples/qwik/src/routes/signup/index.tsx
@@ -1,103 +1,103 @@
-import { component$ } from '@builder.io/qwik';
+import { component$ } from "@builder.io/qwik";
 import {
-  Form,
-  Link,
-  routeAction$,
-  routeLoader$,
-  z,
-  zod$,
-} from '@builder.io/qwik-city';
-import { auth } from '~/lib/lucia';
-import { Prisma } from '@prisma/client';
-import { LuciaError } from 'lucia-auth';
+	Form,
+	Link,
+	routeAction$,
+	routeLoader$,
+	z,
+	zod$
+} from "@builder.io/qwik-city";
+import { auth } from "~/lib/lucia";
+import { Prisma } from "@prisma/client";
+import { LuciaError } from "lucia-auth";
 
 export const useUserLoader = routeLoader$(async (event) => {
-  const authRequest = auth.handleRequest(event);
-  const { user } = await authRequest.validateUser();
-  if (user) throw event.redirect(302, '/');
+	const authRequest = auth.handleRequest(event);
+	const session = await authRequest.validate();
+	if (session) throw event.redirect(302, "/");
 
-  return {};
+	return {};
 });
 
 export const useSignupAction = routeAction$(
-  async (values, event) => {
-    try {
-      const user = await auth.createUser({
-        primaryKey: {
-          providerId: 'username',
-          providerUserId: values.username,
-          password: values.password,
-        },
-        attributes: {
-          username: values.username,
-        },
-      });
-      const session = await auth.createSession(user.userId);
-      const authRequest = auth.handleRequest(event);
-      authRequest.setSession(session);
-    } catch (error) {
-      if (
-        error instanceof Prisma.PrismaClientKnownRequestError &&
-        error.code === 'P2002' &&
-        error.message?.includes('username')
-      ) {
-        return event.fail(500, {
-          error: 'Username already in use',
-        });
-      }
-      if (
-        error instanceof LuciaError &&
-        error.message === 'AUTH_DUPLICATE_KEY_ID'
-      ) {
-        return event.fail(500, {
-          error: 'Username already in use',
-        });
-      }
-      // database connection error
-      console.error(error);
-      return event.fail(500, {
-        error: 'Unknown error occurred',
-      });
-    }
+	async (values, event) => {
+		try {
+			const user = await auth.createUser({
+				primaryKey: {
+					providerId: "username",
+					providerUserId: values.username,
+					password: values.password
+				},
+				attributes: {
+					username: values.username
+				}
+			});
+			const session = await auth.createSession(user.userId);
+			const authRequest = auth.handleRequest(event);
+			authRequest.setSession(session);
+		} catch (error) {
+			if (
+				error instanceof Prisma.PrismaClientKnownRequestError &&
+				error.code === "P2002" &&
+				error.message?.includes("username")
+			) {
+				return event.fail(500, {
+					error: "Username already in use"
+				});
+			}
+			if (
+				error instanceof LuciaError &&
+				error.message === "AUTH_DUPLICATE_KEY_ID"
+			) {
+				return event.fail(500, {
+					error: "Username already in use"
+				});
+			}
+			// database connection error
+			console.error(error);
+			return event.fail(500, {
+				error: "Unknown error occurred"
+			});
+		}
 
-    // if all goes well, redirect to home page
-    throw event.redirect(302, '/');
-  },
-  zod$({
-    username: z.string().min(3).max(20),
-    password: z.string().min(6).max(20),
-  })
+		// if all goes well, redirect to home page
+		throw event.redirect(302, "/");
+	},
+	zod$({
+		username: z.string().min(3).max(20),
+		password: z.string().min(6).max(20)
+	})
 );
 
 export default component$(() => {
-  const signupAction = useSignupAction();
-  console.log(signupAction.value?.failed);
-  return (
-    <>
-      <Form action={signupAction}>
-        <label for="username">username</label>
+	const signupAction = useSignupAction();
 
-        <br />
-        <input id="username" name="username" />
+	return (
+		<>
+			<Form action={signupAction}>
+				<label for="username">username</label>
 
-        <br />
-        <label for="password">password</label>
-        <br />
-        <input type="password" id="password" name="password" />
+				<br />
+				<input id="username" name="username" />
 
-        <br />
-        <input type="submit" value="Continue" class="button" />
-      </Form>
-      {signupAction.value?.failed && (
-        <>
-          <p class="error">{signupAction.value?.error}</p>
-          <p class="error">{signupAction.value.fieldErrors?.password}</p>
-        </>
-      )}
+				<br />
+				<label for="password">password</label>
+				<br />
+				<input type="password" id="password" name="password" />
 
-      <Link href="/login" class="link">
-        Sign in
-      </Link>
-    </>
-  );
+				<br />
+				<input type="submit" value="Continue" class="button" />
+			</Form>
+			{signupAction.value?.failed && (
+				<>
+					<p class="error">{signupAction.value?.error}</p>
+					<p class="error">{signupAction.value.fieldErrors?.password}</p>
+				</>
+			)}
+
+			<Link href="/login" class="link">
+				Sign in
+			</Link>
+		</>
+	);
 });
