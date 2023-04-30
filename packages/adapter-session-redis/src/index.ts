5b785ebe74e41f26f70d18683a91b2b6e1e260f0
scorsi <sylvain.corsini@protonmail.com>
2023-04-30 18:54:16 +0200

    feat(adapter-session-redis): check duplicate session id

--- packages/adapter-session-redis/src/index.ts
+++ packages/adapter-session-redis/src/index.ts
@@ -4,6 +4,7 @@ import type {
 	AdapterFunction
 } from "lucia-auth";
 import type { RedisClientType } from "redis";
+import { LuciaError } from "lucia-auth";
 
 type AdapterSessionRedisOptions = {
 	namespaces?: {
@@ -28,75 +29,84 @@ const adapter =
 			} = {}
 		} = options ?? {};
 
-		return {
-			getSession: async (sessionId) => {
-				const sessionData = await redisClient.get(
-					`${sessionNamespace}:${sessionId}`
-				);
-				if (!sessionData) return null;
-				return JSON.parse(sessionData) as SessionSchema;
-			},
-			getSessionsByUserId: async (userId) => {
-				const sessionIds = await redisClient.lRange(
-					`${userSessionNamespace}:${userId}`,
-					0,
-					-1
-				);
-				const sessionData = await Promise.all(
-					sessionIds.map((id) => redisClient.get(`${sessionNamespace}:${id}`))
-				);
-				return sessionData
-					.filter((val): val is string => val !== null)
-					.map((val) => JSON.parse(val) as SessionSchema);
-			},
-			setSession: async (session) => {
-				await Promise.all([
-					redisClient.lPush(
+		const getSession = async (sessionId: string) => {
+			const sessionData = await redisClient.get(
+				`${sessionNamespace}:${sessionId}`
+			);
+			if (!sessionData) return null;
+			return JSON.parse(sessionData) as SessionSchema;
+		};
+
+		const getSessionsByUserId = async (userId: string) => {
+			const sessionIds = await redisClient.lRange(
+				`${userSessionNamespace}:${userId}`,
+				0,
+				-1
+			);
+			const sessionData = await Promise.all(
+				sessionIds.map((id) => redisClient.get(`${sessionNamespace}:${id}`))
+			);
+			return sessionData
+				.filter((val): val is string => val !== null)
+				.map((val) => JSON.parse(val) as SessionSchema);
+		};
+
+		const setSession = async (session: SessionSchema) => {
+			if (await getSession(session.id))
+				throw new LuciaError("AUTH_DUPLICATE_SESSION_ID");
+
+			await Promise.all([
+				redisClient.lPush(
+					`${userSessionNamespace}:${session.user_id}`,
+					session.id
+				),
+				redisClient.set(
+					`${sessionNamespace}:${session.id}`,
+					JSON.stringify(session),
+					{
+						EX: Math.floor(Number(session.idle_expires) / 1000)
+					}
+				)
+			]);
+		};
+
+		const deleteSession = async (...sessionIds: string[]) => {
+			const targetSessionData = await Promise.all(
+				sessionIds.map((id) => redisClient.get(`${sessionNamespace}:${id}`))
+			);
+			const sessions = targetSessionData
+				.filter((val): val is string => val !== null)
+				.map((val) => JSON.parse(val) as SessionSchema);
+			await Promise.all([
+				...sessionIds.map((id) => redisClient.del(`${sessionNamespace}:${id}`)),
+				...sessions.map((session) =>
+					redisClient.lRem(
 						`${userSessionNamespace}:${session.user_id}`,
+						0,
 						session.id
-					),
-					redisClient.set(
-						`${sessionNamespace}:${session.id}`,
-						JSON.stringify(session),
-						{
-							EX: Math.floor(Number(session.idle_expires) / 1000)
-						}
 					)
-				]);
-			},
-			deleteSession: async (...sessionIds) => {
-				const targetSessionData = await Promise.all(
-					sessionIds.map((id) => redisClient.get(`${sessionNamespace}:${id}`))
-				);
-				const sessions = targetSessionData
-					.filter((val): val is string => val !== null)
-					.map((val) => JSON.parse(val) as SessionSchema);
-				await Promise.all([
-					...sessionIds.map((id) =>
-						redisClient.del(`${sessionNamespace}:${id}`)
-					),
-					...sessions.map((session) =>
-						redisClient.lRem(
-							`${userSessionNamespace}:${session.user_id}`,
-							0,
-							session.id
-						)
-					)
-				]);
-			},
-			deleteSessionsByUserId: async (userId) => {
-				const sessionIds = await redisClient.lRange(
-					`${userSessionNamespace}:${userId}`,
-					0,
-					-1
-				);
-				await Promise.all([
-					...sessionIds.map((id) =>
-						redisClient.del(`${sessionNamespace}:${id}`)
-					),
-					redisClient.del(`${userSessionNamespace}:${userId}`)
-				]);
-			}
+				)
+			]);
+		};
+
+		const deleteSessionsByUserId = async (userId: string) => {
+			const sessionIds = await redisClient.lRange(
+				`${userSessionNamespace}:${userId}`,
+				0,
+				-1
+			);
+			await Promise.all([
+				...sessionIds.map((id) => redisClient.del(`${sessionNamespace}:${id}`)),
+				redisClient.del(`${userSessionNamespace}:${userId}`)
+			]);
+		};
+
+		return {
+			getSession,
+			getSessionsByUserId,
+			setSession,
+			deleteSession,
+			deleteSessionsByUserId
 		};
 	};
 
