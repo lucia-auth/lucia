86eeb0af50342037c4f9c29b5a336ae7d610492d
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2023-05-21 11:38:43 +0900

    format

--- examples/remix/app/routes/api.oauth.ts
+++ examples/remix/app/routes/api.oauth.ts
@@ -2,7 +2,7 @@ import { oauthStateCookie } from "@auth/cookie.server";
 import { githubAuth } from "@auth/lucia.server";
 import { redirect } from "@remix-run/node";
 
-import type {LoaderArgs} from "@remix-run/node"
+import type { LoaderArgs } from "@remix-run/node";
 
 export const loader = async ({ request }: LoaderArgs) => {
 	const url = new URL(request.url);
