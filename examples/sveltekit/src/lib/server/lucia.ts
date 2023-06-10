981b5c07f4361d5cf5d1ca5d3d9ab8ca2f26da55
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2023-06-10 16:45:57 +0900

    update prisma

--- examples/sveltekit/src/lib/server/lucia.ts
+++ examples/sveltekit/src/lib/server/lucia.ts
@@ -10,14 +10,7 @@ import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from '$env/static/private';
 export const auth = lucia({
 	adapter: prisma({
 		client: new PrismaClient(),
-		models: {
-			user: 'user',
-			session: 'session',
-			key: 'key'
-		},
-		tables: {
-			user: 'example_user'
-		}
+		mode: "default"
 	}),
 	env: dev ? 'DEV' : 'PROD',
 	getUserAttributes: (userData) => {
