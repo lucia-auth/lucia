04079efc98bd7f7447633a073bf040ae124a7755
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2023-06-10 15:22:35 +0900

    setup sk

--- examples/sveltekit/src/lib/server/lucia.ts
+++ examples/sveltekit/src/lib/server/lucia.ts
@@ -1,4 +1,4 @@
-import lucia from 'lucia';
+import { lucia } from 'lucia';
 import { sveltekit } from 'lucia/middleware';
 import prisma from '@lucia-auth/adapter-prisma';
 import { dev } from '$app/environment';
@@ -10,9 +10,8 @@ import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from '$env/static/private';
 export const auth = lucia({
 	adapter: prisma(new PrismaClient()),
 	env: dev ? 'DEV' : 'PROD',
-	transformDatabaseUser: (userData) => {
+	getUserAttributes: (userData) => {
 		return {
-			userId: userData.id,
 			username: userData.username
 		};
 	},
