4fad93d50211db13f747d987e5e89ecc47d8e50c
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2023-06-18 14:10:57 +0900

    adjust code font size

--- documentation-v2/tailwind.config.cjs
+++ documentation-v2/tailwind.config.cjs
@@ -1,17 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {
 			colors: {
-				main: "#5f57ff",
+				main: "#5f57ff"
 			},
 			fontSize: {
 				"code-sm": "0.825rem",
 				"code-base": "0.925rem",
+				"code-lg": "1.12rem",
+				"code-xl": "1.2rem",
+				"code-2xl": "1.45rem",
+				"code-3xl": "1.825rem",
+				"code-4xl": "2.15rem",
 				"code-5xl": "2.9rem"
 			}
-		},
+		}
 	},
-	plugins: [],
-}
+	plugins: []
+};
