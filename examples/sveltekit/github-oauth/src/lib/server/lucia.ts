c931f862a243c05055afc4b53103b8305bf89542
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2023-07-04 11:17:41 +0900

    add sveltekit oauth example

---
+++ examples/sveltekit/github-oauth/src/lib/server/lucia.ts
@@ -0,0 +1,31 @@
+import { lucia } from 'lucia';
+import { betterSqlite3 } from '@lucia-auth/adapter-sqlite';
+import { sveltekit } from 'lucia/middleware';
+import {github} from "@lucia-auth/oauth/providers"
+import { dev } from '$app/environment';
+import sqlite from 'better-sqlite3';
+import {GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET} from "$env/static/private"
+
+const db = sqlite('main.db');
+
+export const auth = lucia({
+	adapter: betterSqlite3(db, {
+		user: 'user',
+		session: 'user_session',
+		key: 'user_key'
+	}),
+	middleware: sveltekit(),
+	env: dev ? 'DEV' : 'PROD',
+	getUserAttributes: (data) => {
+		return {
+			githubUsername: data.github_username
+		};
+	}
+});
+
+export const githubAuth = github(auth, {
+	clientId: GITHUB_CLIENT_ID,
+	clientSecret: GITHUB_CLIENT_SECRET,
+})
+
+export type Auth = typeof auth;
