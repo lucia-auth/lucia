7e4ba628f70034ae0b9eeb64de756b43153e0172
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2023-07-05 21:17:22 +0900

    remove changes to examples

--- examples/nuxt/middleware/auth.ts
+++
@@ -1,8 +0,0 @@
-export default defineNuxtRouteMiddleware(async () => {
-	const { user, fetchUser } = useAuth();
-	await fetchUser();
-	if (!user.value) {
-		// if there's no user found, navigate to the login page.
-		return navigateTo("/login");
-	}
-});
--- test-apps/nuxt/composables/useAuth.ts
+++
@@ -1,6 +0,0 @@
-export const useAuth = () => {
-	const { data, execute: fetchUser } = useFetch("/api/user", {
-		immediate: false
-	});
-	return { user: computed(() => data.value?.user), fetchUser };
-};
