4ba9450a42b1dcc677f3e50bcfd24959910393a4
pilcrowOnPaper <80624252+pilcrowOnPaper@users.noreply.github.com>
2023-07-23 13:13:35 +0900

    Export Lucia hash function (#864)

--- packages/lucia/src/utils/index.ts
+++ packages/lucia/src/utils/index.ts
@@ -1,3 +1,7 @@
 export { generateRandomString } from "./nanoid.js";
 export { serializeCookie, parseCookie } from "./cookie.js";
 export { isWithinExpiration } from "./date.js";
+export {
+	generateScryptHash as generateLuciaPasswordHash,
+	validateScryptHash as validateLuciaPasswordHash
+} from "./crypto.js";
