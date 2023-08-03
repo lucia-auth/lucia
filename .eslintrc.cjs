2dc3672a5375494efd6570c66bf02486ff059e60
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2023-08-03 22:46:27 +0900

    remove unused prettier and eslint plugins

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,10 +15,6 @@ module.exports = {
 		"plugin:@typescript-eslint/recommended",
 		"prettier"
 	],
-	plugins: ["svelte3", "@typescript-eslint"],
-	ignorePatterns: ["*.cjs"],
-	overrides: [{ files: ["*.svelte"], processor: "svelte3/svelte3" }],
-	settings: {
-		"svelte3/typescript": () => require("typescript")
-	}
+	plugins: ["@typescript-eslint"],
+	ignorePatterns: ["*.cjs"]
 };
