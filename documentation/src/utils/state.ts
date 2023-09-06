0bdf2e96f95da926f8e32936a57bbfa68683b2b9
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2023-09-06 21:06:08 +0900

    update state management

--- documentation/src/utils/state.ts
+++ documentation/src/utils/state.ts
@@ -1,54 +1,35 @@
-type Callback = (state: boolean) => any;
+type Callback<T> = (state: T) => any;
 
 export const createToggleState = () => {
-	const callbacks: Callback[] = [];
-	let internal = false;
-
-	const onToggle = (callback: Callback) => {
-		callbacks.push(callback);
-	};
-
-	const toggle = () => {
-		internal = !internal;
-		for (const callback of callbacks) {
-			callback(internal);
-		}
-	};
-
-	return [toggle, onToggle] as const;
+	const [state, onUpdate] = createState(false);
+	const toggleState = {
+		value: state.value,
+		toggle: () => state.set(!state.value())
+	} as const;
+	return [toggleState, onUpdate] as const;
 };
 
-export const createState = (defaultState = false) => {
-	const callbacks: Callback[] = [];
-	let internal = defaultState;
+export const createState = <T>(initialState: T) => {
+	const callbacks: Callback<T>[] = [];
+	let internal = initialState;
 
-	const onUpdate = (callback: Callback) => {
+	const onUpdate = (callback: Callback<T>) => {
 		callbacks.push(callback);
 	};
-
-	const state = (value?: boolean) => {
-		if (value !== undefined) {
-			internal = value;
-			for (const callback of callbacks) {
-				callback(internal);
+	const state = {
+		value: () => internal,
+		set: (value: T) => {
+			if (value !== undefined) {
+				internal = value;
+				for (const callback of callbacks) {
+					callback(internal);
+				}
 			}
+			return internal;
 		}
-		return internal;
-	};
-
+	} as const;
 	return [state, onUpdate] as const;
 };
 
-export const [toggleMenu, onMenuToggle] = createToggleState();
-export const [searchVisibility, onSearchVisibilityUpdate] = createState();
-
-export const setElementVisibility = (
-	element: HTMLElement,
-	visible: boolean
-) => {
-	if (visible) {
-		element.classList.remove("hidden");
-	} else {
-		element.classList.add("hidden");
-	}
-};
+export const [menuVisible, onMenuToggle] = createToggleState();
+export const [searchVisible, onSearchVisibilityUpdate] = createState(false);
