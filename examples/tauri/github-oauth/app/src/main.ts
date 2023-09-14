f4a492796235e0a5fce3e5973a80d30875a98fcc
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2023-09-14 13:17:04 +0900

    update tauri example

--- examples/tauri/github-oauth/app/src/main.ts
+++ examples/tauri/github-oauth/app/src/main.ts
@@ -1,36 +1,50 @@
 import { invoke } from "@tauri-apps/api/tauri";
 import { getClient, ResponseType } from "@tauri-apps/api/http";
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("DOMContentLoaded", async () => {
 	const loginButton = document.getElementById("login-button");
 	const profileBody = document.getElementById("profile");
 	const logoutButton = document.getElementById("logout-button");
 	if (!loginButton || !profileBody || !logoutButton) return;
+
+	const renderUserProfile = (user: User | null) => {
+		profileBody.textContent = user ? JSON.stringify(user, null, 2) : "(none)";
+	};
+
 	loginButton.addEventListener("click", async () => {
 		try {
 			const sessionToken = await invoke<string>("authenticate");
 			localStorage.setItem("session_token", sessionToken);
 			const user = await getUser(sessionToken);
-			profileBody.textContent = JSON.stringify(user, null, 2);
+			renderUserProfile(user);
 		} catch (e) {
 			console.log(e);
 		}
 	});
 	logoutButton.addEventListener("click", async () => {
 		const sessionToken = localStorage.getItem("session_token");
-    if (!sessionToken) return 
+		if (!sessionToken) return;
 		const client = await getClient();
 		const response = await client.request({
-      url: "http://localhost:3000/logout",
-      method: "POST",
+			url: "http://localhost:3000/logout",
+			method: "POST",
 			headers: {
 				Authorization: `Bearer ${sessionToken}`
 			}
 		});
-    if (!response.ok) return
-    localStorage.removeItem("session_token");
-    profileBody.textContent = "(none)"
+		if (!response.ok) return;
+		localStorage.removeItem("session_token");
+		renderUserProfile(null);
 	});
+
+	const sessionToken = localStorage.getItem("session_token");
+	if (sessionToken) {
+		const user = await getUser(sessionToken);
+		if (!user) {
+			localStorage.removeItem("session_token");
+		}
+		renderUserProfile(user);
+	}
 });
 
 const getUser = async (sessionToken: string): Promise<User | null> => {
