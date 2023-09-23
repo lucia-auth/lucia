100c6c25bbc63ca5c14f99e3ec0582a63c804bb0
SkepticMystic <70717676+SkepticMystic@users.noreply.github.com>
2023-09-23 14:20:25 +0200

    Create joinAdapter util (#1081)
    Co-authored-by: pilcrowOnPaper <80624252+pilcrowOnPaper@users.noreply.github.com>
    Co-authored-by: lucythecat <pilcrowonpaper@gmail.com>

---
+++ packages/lucia/src/utils/adapter.ts
@@ -0,0 +1,22 @@
+import type { Adapter, InitializeAdapter, SessionAdapter, UserAdapter } from "../index.js";
+
+export const joinAdapters = (
+  baseAdapter: InitializeAdapter<Adapter | SessionAdapter | UserAdapter>,
+  ...adapters: Array<
+    | Partial<Adapter>
+    | InitializeAdapter<Adapter |SessionAdapter | UserAdapter>
+  >
+): InitializeAdapter<Adapter> => {
+  return (LuciaError) => {
+    return Object.assign(
+      // start with the baseAdapter
+      baseAdapter(LuciaError),
+      // merge in the partial adapters
+      ...adapters.map((adapter) => {
+        if (typeof adapter === "function") return adapter(LuciaError);
+        return adapter;
+      }),
+    );
+  }
+}
+  
