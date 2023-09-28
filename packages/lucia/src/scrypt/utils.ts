bb857c2fd7224a2fd989b1b780090fb84d08ca43
pilcrowOnPaper <80624252+pilcrowOnPaper@users.noreply.github.com>
2023-09-29 08:38:33 +0900

    Add proper attribution for `@noble/hashes` (#1171)

--- packages/lucia/src/scrypt/utils.ts
+++ packages/lucia/src/scrypt/utils.ts
@@ -1,7 +1,12 @@
-export const u32 = (arr: Uint8Array) =>
-	new Uint32Array(arr.buffer, arr.byteOffset, Math.floor(arr.byteLength / 4));
+export const u32 = (arr: Uint8Array): Uint32Array => {
+	return new Uint32Array(
+		arr.buffer,
+		arr.byteOffset,
+		Math.floor(arr.byteLength / 4)
+	);
+};
 
-export const nextTick = async () => {};
+export const nextTick = async (): Promise<void> => {};
 
 export function checkOpts<T1 extends {}, T2 extends {}>(
 	defaults: T1,
@@ -14,11 +19,11 @@ export function checkOpts<T1 extends {}, T2 extends {}>(
 export const asyncLoop = async (
 	iters: number,
 	tick: number,
-	cb: (i: number) => void
-) => {
+	callback: (i: number) => void
+): Promise<void> => {
 	let ts = Date.now();
 	for (let i = 0; i < iters; i++) {
-		cb(i);
+		callback(i);
 		const diff = Date.now() - ts;
 		if (diff >= 0 && diff < tick) continue;
 		await nextTick();
