ab86ffdf808cfc5ead61be5989b38a8936f4858c
Edik <eduard221@web.de>
2023-10-07 18:25:35 +0200

    completed Type KeycloakUser

--- packages/oauth/src/providers/keycloak.ts
+++ packages/oauth/src/providers/keycloak.ts
@@ -223,15 +223,28 @@ export type Claims = {
 }
 
 export type KeycloakUser = {
-  sub: string;  // user_id
-  name: string;
-  preferred_username: string;
-  given_name: string;
-  locale: string;
-  family_name: string;
-  email: string;
-  email_verified: boolean;
-};
+    exp: number;
+    iat: number;
+    auth_time: number;
+    jti: string;
+    iss: string;
+    aud: string;
+    sub: string;  // user_id
+    typ: string;
+    azp: string;
+    session_state: string;
+    at_hash: string;
+    acr: string;
+    sid: string;
+    email_verified: boolean;
+    name: string;
+    preferred_username: string;
+    given_name: string;
+    family_name: string;
+    email: string;
+    picture: string;
+    user: any;
+}
 
 export type KeycloakRole = {
 	type: "realm" | "resource",
