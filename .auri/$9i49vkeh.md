4c02aacbacc315f0e9af911d5d2bfad9962c13cd
lucythecat <pilcrowonpaper@gmail.com>
2023-11-06 12:19:46 +0900

    revert #1158

---
+++ .auri/$9i49vkeh.md
@@ -0,0 +1,6 @@
+---
+package: "lucia" # package name
+type: "patch" # "major", "minor", "patch"
+---
+
+Revert #1158
--- packages/lucia/src/utils/crypto.ts
+++ packages/lucia/src/utils/crypto.ts
@@ -1,11 +1,21 @@
 import { LuciaError } from "../auth/error.js";
-import { scryptAsync as scrypt } from "@noble/hashes/scrypt";
-import { customAlphabet } from "nanoid";
+import { scrypt } from "../scrypt/index.js";
 
-export const generateRandomString = (length: number, alphabet?: string) => {
-	const DEFAULT_ALPHABET = "abcdefghijklmnopqrstuvwxyz1234567890";
-	const customNanoid = customAlphabet(alphabet ?? DEFAULT_ALPHABET);
-	return customNanoid(length);
+const DEFAULT_ALPHABET = "abcdefghijklmnopqrstuvwxyz1234567890";
+
+export const generateRandomString = (
+	length: number,
+	alphabet: string = DEFAULT_ALPHABET
+) => {
+	const randomUint32Values = new Uint32Array(length);
+	crypto.getRandomValues(randomUint32Values);
+	const u32Max = 0xffffffff;
+	let result = "";
+	for (let i = 0; i < randomUint32Values.length; i++) {
+		const rand = randomUint32Values[i] / (u32Max + 1);
+		result += alphabet[Math.floor(alphabet.length * rand)];
+	}
+	return result;
 };
 
 export const generateScryptHash = async (s: string): Promise<string> => {
