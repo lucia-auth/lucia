3c38594cf38b625b6d59f215580aed72ad2130e1
lucythecat <pilcrowonpaper@gmail.com>
2023-11-20 14:59:09 +0900

    remove session adapters and readonly middleware

--- packages/adapter-session-redis/src/index.ts
+++
@@ -1,3 +0,0 @@
-export { RedisSessionAdapter } from "./drivers/redis.js";
-export { UpstashSessionAdapter } from "./drivers/upstash.js";
-export { IoredisSessionAdapter } from "./drivers/ioredis.js";
