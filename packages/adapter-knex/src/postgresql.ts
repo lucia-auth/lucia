e9b7eb44626417eaa03dba50c9a0a21e0826feb5
shig07770 <shig07770+dev@gmail.com>
2024-04-12 10:19:58 +0400

    refactor(adapter-knex): move shared code to a base file

--- packages/adapter-knex/src/postgresql.ts
+++ packages/adapter-knex/src/postgresql.ts
@@ -1,42 +1,7 @@
-import type { Knex } from "knex";
-import type { Adapter, DatabaseSession, DatabaseUser, UserId } from "lucia";
-
-type KnexPostgreSQLAdapterTables = ({
-  users: string;
-  sessions: string;
-});
-
-type KnexUserTable = ({
-  userId: UserId;
-});
-
-type KnexSessionTable = ({
-  userId: UserId;
-  sessionId: string;
-  expiresAtTimestamp: Date;
-});
-
-function transformIntoDatabaseSession(raw: KnexSessionTable): DatabaseSession {
-  const { sessionId, userId, expiresAtTimestamp, ...attributes } = raw;
-  const expiresAt = new Date(expiresAtTimestamp);
-
-  return ({ id: sessionId, userId, expiresAt, attributes });
-}
-
-function transformIntoDatabaseUser(raw: KnexUserTable): DatabaseUser {
-  const { userId, ...attributes } = raw;
-  return ({ id: userId, attributes });
-}
-
-export default class KnexPostgreSQLAdapter implements Adapter {
-  private readonly knex: Knex;
-  private readonly tables: KnexPostgreSQLAdapterTables;
-
-  public constructor(knex: Knex, tables: KnexPostgreSQLAdapterTables) {
-    this.knex = knex;
-    this.tables = tables;
-  }
-
+import BaseKnexAdapter from "./base.js";
+import type { KnexUserTable, KnexSessionTable } from "./base.js";
+import type { Adapter, DatabaseSession, DatabaseUser } from "lucia";
+export default class KnexMySQLAdapter extends BaseKnexAdapter implements Adapter {
   public async getSessionAndUser(sessionId: string): Promise<[session: DatabaseSession | null, user: DatabaseUser | null]> {
     const [result] = ((
       await this.knex<KnexSessionTable>(this.tables.sessions)
@@ -52,51 +17,9 @@ export default class KnexPostgreSQLAdapter implements Adapter {
       return [null, null];
     }
 
-    const user = transformIntoDatabaseUser(result.user);
-    const session = transformIntoDatabaseSession(result.session);
+    const user = this.transformIntoDatabaseUser(result.user);
+    const session = this.transformIntoDatabaseSession(result.session);
 
     return [session, user];
-  }
-
-  public async getUserSessions(userId: string): Promise<DatabaseSession[]> {
-    const results = await this.knex<KnexSessionTable>(this.tables.sessions)
-      .select("*")
-      .where("userId", "=", userId);
-
-    return results.map((result) => transformIntoDatabaseSession(result));
-  }
-
-  public async setSession(session: DatabaseSession): Promise<void> {
-    await this.knex<KnexSessionTable>(this.tables.sessions)
-      .insert({
-        sessionId: session.id,
-        userId: session.userId,
-        expiresAtTimestamp: session.expiresAt,
-        ...session.attributes
-      });
-  }
-
-  public async updateSessionExpiration(sessionId: string, expiresAtTimestamp: Date): Promise<void> {
-    await this.knex<KnexSessionTable>(this.tables.sessions)
-      .update("expiresAtTimestamp", expiresAtTimestamp)
-      .where("sessionId", "=", sessionId);
-  }
-
-  public async deleteSession(sessionId: string): Promise<void> {
-    await this.knex<KnexSessionTable>(this.tables.sessions)
-      .del()
-      .where("sessionId", "=", sessionId);
-  }
-
-  public async deleteUserSessions(userId: string): Promise<void> {
-    await this.knex<KnexSessionTable>(this.tables.sessions)
-      .del()
-      .where("userId", "=", userId);
-  }
-
-  public async deleteExpiredSessions(): Promise<void> {
-    await this.knex<KnexSessionTable>(this.tables.sessions)
-      .del()
-      .where("expiresAtTimestamp", "<=", new Date());
   }
 }
